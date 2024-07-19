a7aa5c4a6c4c2dcf9343f4f2537cc4617e818550
umaid-ca <umaid@codeautomation.ai>
2024-07-20 00:14:26 +0500

    resolved error

--- src/app/components/stock_list/Stock_details.js
+++ src/app/components/stock_list/Stock_details.js
@@ -1,31 +1,31 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import LineChart from "./LinkChart";
 
 const Stock_details = () => {
-  const { data: session, status } = useSession()
-  const router = useRouter()
-  const [indiceData, setIndicedata] = useState(undefined)
-  const [selectedData, useSelectedData ] = useState(null)
-  const [percentagePorL, setPercentagePorL] = useState("0")
-  const [pricePorL, setPricePorL] = useState("0")
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [indiceData, setIndicedata] = useState(undefined);
+  const [selectedData, setSelectedData] = useState(null);
+  const [percentagePorL, setPercentagePorL] = useState("0");
+  const [pricePorL, setPricePorL] = useState("0");
 
-  const color = percentagePorL > 0 ? 'green' : 'red';
+  const color = percentagePorL > 0 ? "green" : "red";
   const textStyle = {
     color: color,
   };
 
   const profitCalculation = (openPrice, closePrice) => {
     let percentageChange = ((closePrice - openPrice) / openPrice) * 100;
-    return percentageChange.toFixed(2)
-  }
+    return percentageChange.toFixed(2);
+  };
 
   const profitPriceCalculation = (openPrice, closePrice) => {
-    let percentageChange = closePrice - openPrice
-    return percentageChange.toFixed(2)
-  }
+    let percentageChange = closePrice - openPrice;
+    return percentageChange.toFixed(2);
+  };
 
   async function getData() {
     const url = `https://api.polygon.io/v2/aggs/ticker/I:${selectedData.symbol}/prev?apiKey=3N1dIVStpI_dSwIpJJZqN6mgneHj4O0p`;
@@ -34,58 +34,71 @@ const Stock_details = () => {
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
-  
+
       const respData = await response.json();
-      let claculation = profitCalculation(respData.results[0].o, respData.results[0].c)
-      let claculationPrice = profitPriceCalculation(respData.results[0].o, respData.results[0].c)
-      setPercentagePorL(claculation)
-      setPricePorL(claculationPrice)
-      setIndicedata(respData.results[0])
-      return respData.results
+      let claculation = profitCalculation(
+        respData.results[0].o,
+        respData.results[0].c
+      );
+      let claculationPrice = profitPriceCalculation(
+        respData.results[0].o,
+        respData.results[0].c
+      );
+      setPercentagePorL(claculation);
+      setPricePorL(claculationPrice);
+      setIndicedata(respData.results[0]);
+      return respData.results;
     } catch (error) {
       console.error(error.message);
     }
   }
 
   useEffect(() => {
-    useSelectedData(JSON.parse(localStorage.getItem("Indice")))
-  }, [])
+    setSelectedData(JSON.parse(localStorage.getItem("Indice")));
+  }, []);
 
   useEffect(() => {
-      if(selectedData) {
-        getData()
-      }
-   
-  }, [selectedData])
+    if (selectedData) {
+      getData();
+    }
+  }, [selectedData]);
 
- 
-      if (status == "loading") {
-        return <h1>Loading...</h1>
-    }else if(status == "authenticated") {
-  return (
-    <div style={{padding: "0px 20px"}}>
-      <button onClick={() => router.push("/")} className="back_btn">{"< Back"}</button>
-      <h1>Stock Indice: {selectedData ? selectedData.name : "Now Selected Data"}</h1>
-      <h2>You may be interested in: </h2>
-      <div className="stock_data">
-      <span>{selectedData?.symbol}<span style={{marginLeft: '30px'}}>{selectedData?.name}</span></span>
-      <span>${indiceData?.c?.toFixed(2)}</span>
-      <span style={textStyle}>{percentagePorL}%</span>
-      <span style={textStyle}>${pricePorL}</span>
-      </div>
-     {indiceData != undefined && <div>
-        <LineChart />
-      </div>}
-      {indiceData == undefined &&  <div>
+  if (status == "loading") {
+    return <h1>Loading...</h1>;
+  } else if (status == "authenticated") {
+    return (
+      <div style={{ padding: "0px 20px" }}>
+        <button onClick={() => router.push("/")} className="back_btn">
+          {"< Back"}
+        </button>
         <h1>
-        No Record Found
+          Stock Indice: {selectedData ? selectedData.name : "Now Selected Data"}
         </h1>
-      </div>}
+        <h2>You may be interested in: </h2>
+        <div className="stock_data">
+          <span>
+            {selectedData?.symbol}
+            <span style={{ marginLeft: "30px" }}>{selectedData?.name}</span>
+          </span>
+          <span>${indiceData?.c?.toFixed(2)}</span>
+          <span style={textStyle}>{percentagePorL}%</span>
+          <span style={textStyle}>${pricePorL}</span>
+        </div>
+        {indiceData != undefined && (
+          <div>
+            <LineChart />
+          </div>
+        )}
+        {indiceData == undefined && (
+          <div>
+            <h1>No Record Found</h1>
+          </div>
+        )}
       </div>
-  )
-} else if(status == "unauthenticated") {
-  return router.push("/login")
-} 
-}
+    );
+  } else if (status == "unauthenticated") {
+    return router.push("/login");
+  }
+};
 
-export default Stock_details
+export default Stock_details;
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -7,49 +7,61 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default function Login() {
+  // const session = await getServerSession(authOptions)
+  // if (session) redirect("/")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
 
-    // const session = await getServerSession(authOptions)
-    // if (session) redirect("/")
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const router = useRouter()
+  const handleSubmit = async (e) => {
+    console.log(email, password, `email password`);
+    e.preventDefault();
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+      console.log(res, `response from login api`);
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
 
-    const handleSubmit = async (e) => {
-        console.log(email, password, `email password`)
-        e.preventDefault()
-        try {
-            const res = await signIn("credentials", {
-              email,
-              password,
-              redirect: false,
-            });
-            console.log(res, `response from login api`)
-            if (res.error) {
-              setError("Invalid Credentials");
-              return;
-            }
-      
-            router.replace("/");
-          } catch (error) {
-            console.log(error);
-          }
+      router.replace("/");
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <main style={{height: '100vh'}} className="body_login_form">
-        <form onSubmit={handleSubmit} className="login">
+  };
+  return (
+    <main style={{ height: "100vh" }} className="body_login_form">
+      <form onSubmit={handleSubmit} className="login">
         <h2>Welcome!</h2>
-        <p style={{color: "black", fontWeight: "700"}}>Please log in</p>
-        <input  onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
-        <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
-        <input style={{borderRadius: '8px'}} type="submit" value="Log In" />
+        <p style={{ color: "black", fontWeight: "700" }}>Please log in</p>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <input style={{ borderRadius: "8px" }} type="submit" value="Log In" />
         {error && (
-              <div style={{margin: "20px 0px"}}>
-              <span  className="error">{error}</span>
-            </div>)}
+          <div style={{ margin: "20px 0px" }}>
+            <span className="error">{error}</span>
+          </div>
+        )}
         <Link href={"/sign_up"}>
-            <span style={{color: "black", fontWeight: "700"}}>Don't have an account? <span className="underline">Register Here!</span></span>
-          </Link>
+          <span style={{ color: "black", fontWeight: "700" }}>
+            Don&apos;t have an account?{" "}
+            <span className="underline">Register Here!</span>
+          </span>
+        </Link>
       </form>
-      </main>
-    )}
+    </main>
+  );
+}
